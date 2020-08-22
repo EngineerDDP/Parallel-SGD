40308d87add9d03c24297710d3d0402f68257b77
Engineer_DDP <zzaddp@live.com>
2020-08-22 18:48:10 +0800

    Change file name format for log file.

--- utils/log.py
+++ utils/log.py
@@ -9,7 +9,7 @@ class Logger:
         self.Title = title_info
         self.ToFile = log_to_file
         self.Folder = './tmp_log/'
-        self.File_Name = self.Folder + '{}_{}.log'.format(title_info, datetime.datetime.now().strftime('%H-%M-%S'))
+        self.File_Name = self.Folder + '{} {}.log'.format(title_info, datetime.datetime.now().strftime('%Y-%m-%d %H%M'))
 
     def log_message(self, msg):
 
