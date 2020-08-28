97f85cceaf44701b61b7642e722d25a9baa20af6
Engineer_DDP <zzaddp@live.com>
2020-08-28 10:12:42 +0800

    Simplicity myExe

--- executor/interfaces.py
+++ executor/interfaces.py
@@ -68,3 +68,29 @@ class IExecutor(metaclass=ABCMeta):
             Return the filename list or executing trace.
         """
         pass
+
+
+class AbsSimpleExecutor(IExecutor):
+
+    def add_info(self, obj: IServerModel):
+        pass
+
+    def add_data(self, obj: IDataset):
+        pass
+
+    def add_setting(self, obj: Settings):
+        pass
+
+    def ready(self) -> bool:
+        return True
+
+    def done(self) -> bool:
+        return True
+
+    @abstractmethod
+    def run(self, com: ICommunication_Controller) -> None:
+        pass
+
+    @abstractmethod
+    def trace_files(self) -> list:
+        pass
--- executor/myExe.py
+++ executor/myExe.py
@@ -1,34 +1,19 @@
 from dataset.interfaces import IDataset
-from executor.interfaces import IExecutor
+from executor.interfaces import AbsSimpleExecutor
 from models.local import IServerModel
 from network.interfaces import ICommunication_Controller
 from profiles import Settings
 
 
-class myExecutor(IExecutor):
+class myExecutor(AbsSimpleExecutor):
 
-    def __init__(self, node_id):
-        super().__init__(node_id)
+    def __init__(self, node_id, offset):
+        super().__init__(node_id, offset)
         self.__my_output_file = "./output.txt"
-
-    def add_info(self, obj: IServerModel):
-        pass
-
-    def add_data(self, obj: IDataset):
-        pass
-
-    def add_setting(self, obj: Settings):
-        pass
 
     def run(self, com: ICommunication_Controller) -> None:
         with open(self.__my_output_file, 'a') as f:
             f.write('Hello world!')
-
-    def ready(self) -> bool:
-        return True
-
-    def done(self) -> bool:
-        return True
 
     def trace_files(self) -> list:
         return [self.__my_output_file]
