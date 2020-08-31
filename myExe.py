bd97e77cd3faf32adb9a80c323ab491bb66f289f
Engineer_DDP <zzaddp@live.com>
2020-08-31 09:57:26 +0800

    Add tutorial for customized executor.

--- myExe.py
+++ myExe.py
@@ -1,19 +1,28 @@
 from executor.interfaces import AbsSimpleExecutor
 from network.interfaces import ICommunication_Controller
 
-
+# 继承自 AbsSimpleExecutor
 class myExecutor(AbsSimpleExecutor):
 
     def __init__(self, node_id, offset):
+        """
+            Node id 是分配到的全局node_id，offset是本组起始id的值
+        """
         super().__init__(node_id, offset)
-        self.__my_output_file = "./HelloWorld"
+        # 计算结果以文件的形式记录
+        self.__my_output_file = "./HelloWorld.txt"
 
     def run(self, com: ICommunication_Controller) -> None:
-        with open(self.__my_output_file, 'a') as f:
-            import os
-
-            f.write("{}".format(os.times()))
+        """
+            获取 ICommunication_Controller 控制权，开始执行任务。
+        """
+        with open(self.__my_output_file, 'w+') as f:
+            for i in range(1, 21):
+                f.write("{} ".format(i))
 
     def trace_files(self) -> list:
+        """
+            以文件的形式返回执行结果
+        """
         return [self.__my_output_file]
 
--- my_submit.py
+++ my_submit.py
@@ -1,17 +1,26 @@
 if __name__ == '__main__':
 
-    from network import NodeAssignment, Request
+    # 导入节点分配容器
+    from network import NodeAssignment
+    # 导入请求处理
+    from network import Request
+    # 导入自定义执行类型，注意：自定义类型不能和下面的代码放在一起
     from myExe import myExecutor
 
+    # 添加一个ip作为Worker
     nodes = NodeAssignment()
     nodes.add(0, '10.196.83.205')
     net = Request()
 
-    from roles import Coordinator, Reclaimer
+    # 增加协调者角色
+    from roles import Coordinator
+
+    # 发起一个请求
     with net.request(nodes) as req:
+        # 在请求的集群上创建一个协调者
         master = Coordinator(req)
+        # 提交任务
         master.submit_job(myExecutor)
+        # 等待执行完成
         master.join()
-        # master = Reclaimer(req)
-        # master.require_client_log()
 
