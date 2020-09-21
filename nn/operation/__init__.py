1d934dfff62a6250d7eb6e59668bb1dd801badeb
chenruifeng <ruifeng.chen.cn@gmail.com>
2020-09-21 19:36:47 +0800

    Fix relu bug

--- nn/operation/__init__.py
+++ nn/operation/__init__.py
@@ -1,5 +1,6 @@
 # for general usage
 from nn.operation.add import Add
+from nn.operation.sub import Sub
 from nn.operation.multiply import Multiply
 from nn.operation.tanh import Tanh
 from nn.operation.relu import ReLU, LeakReLU
