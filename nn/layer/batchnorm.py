0e67d6ca1440792b57eef8348f6fe47e31f88a83
chenruifeng <ruifeng.chen.cn@gmail.com>
2020-10-13 18:25:37 +0800

    fix barchnorm layer.

--- nn/layer/batchnorm.py
+++ nn/layer/batchnorm.py
@@ -10,23 +10,25 @@ class BatchNorm(AbsLayer):
     def __init__(self, activation: IActivation = None, inputs: IOperator = None):
         super().__init__(inputs, activation)
         self.__shape = None
-        self.__mu = None
-        self.__sigma = None
+        self.__mu = 0
+        self.__sigma = 1
         self.__eps = 1e-7
+        self.__momentum = 0.7
 
     def output_shape(self) -> [list, tuple, None]:
         return self.__shape
 
     @property
     def variables(self) -> tuple:
-        return self.__mu, self.__sigma
+        return ()
 
     def initialize_parameters(self, x) -> None:
         self.__shape = x.shape
 
     def do_forward_predict(self, x):
-        self.__mu = x.mean(axis=0)
-        self.__sigma = x.var(axis=0)
+        self.__mu = self.__momentum * self.__mu + (1 - self.__momentum) * x.mean(axis=0)
+        self.__sigma = self.__momentum * self.__sigma + (1 - self.__momentum) * x .var(axis=0)
+        print(self.__sigma)
         return (x - self.__mu) / np.sqrt(self.__sigma + self.__eps)
 
     def do_forward_train(self, x):
@@ -36,7 +38,7 @@ class BatchNorm(AbsLayer):
         pass
 
     def backward_propagate(self, grad):
-        return grad * np.sqrt(self.__sigma + self.__eps) + self.__mu
+        return grad / np.sqrt(self.__sigma + self.__eps)
 
     def __str__(self):
         return "<BatchNorm Layer>"
