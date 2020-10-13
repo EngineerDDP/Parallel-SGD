4bc69ffc57e0fd9f62662699e05339e4f9de0d49
Engineer_DDP <zzaddp@live.com>
2020-10-13 19:11:54 +0800

    Stabilize cross entropy calculation.

--- nn/loss/cross_entropy.py
+++ nn/loss/cross_entropy.py
@@ -7,16 +7,18 @@ from nn.loss.abstract import ILoss
 class Cross_Entropy(ILoss):
 
     def __init__(self):
-        pass
+        self.__epsilon = 1e-9
 
     def __repr__(self):
-        print(self.__str__())
+        return self.__str__()
 
     def __str__(self):
         return "<Cross Entropy Loss>"
 
     def metric(self, arg1, arg2):
-        return np.mean(np.multiply(arg2, np.log(arg1)) + np.multiply((1 - arg2), np.log(1 - arg1))) / -1
+        q = np.multiply(arg2, np.log(arg1 + self.__epsilon))
+        p = np.multiply((1 - arg2), np.log(1 - arg1 + self.__epsilon))
+        return -1 * np.mean(np.sum(q + p, axis=1))
 
     def gradient(self, left: [float, ndarray], right: [float, ndarray]) -> (ndarray, ndarray):
         return (1 - right) / (1 - left) - right / left, (1 - left) / (1 - right) - left / right
@@ -25,16 +27,18 @@ class Cross_Entropy(ILoss):
 class Cross_Entropy_With_Softmax(ILoss):
 
     def __init__(self):
-        pass
+        self.__epsilon = 1e-9
 
     def __repr__(self):
-        print(self.__str__())
+        return self.__str__()
 
     def __str__(self):
         return "<Cross Entropy Loss>"
 
     def metric(self, arg1, arg2):
-        return np.mean(np.multiply(arg2, np.log(arg1)) + np.multiply((1 - arg2), np.log(1 - arg1))) / -1
+        q = np.multiply(arg2, np.log(arg1 + self.__epsilon))
+        p = np.multiply((1 - arg2), np.log(1 - arg1 + self.__epsilon))
+        return -1 * np.mean(np.sum(q + p, axis=1))
 
     def gradient(self, arg1, arg2):
         return arg1 - arg2, arg2 - arg1
