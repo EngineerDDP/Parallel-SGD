a0d18e4e0603faa0244e98aa25b9816233b53daa
Engineer_DDP <zzaddp@live.com>
2020-10-13 19:45:21 +0800

    Reformat file.

--- nn/optimizer/parameter_average.py
+++ nn/optimizer/parameter_average.py
@@ -10,13 +10,13 @@ class PAOptimizer(IOptimizer):
         Interact with transfer.
     """
 
-    def __init__(self, gradient_descent:IGradientDescent, transfer:ITransfer, block_mgr:IPSGDBlockMgr):
+    def __init__(self, gradient_descent: IGradientDescent, transfer: ITransfer, block_mgr: IPSGDBlockMgr):
         self.__transfer = transfer
         self.__block_mgr = block_mgr
         self.__optimizer = gradient_descent
         self.__batch_size = 1
 
-    def optimize(self, variable:ITrainable):
+    def optimize(self, variable: ITrainable):
         """
             1st order gradient based optimize algorithm.
             {arg min}_{x}{F(x)}
@@ -27,7 +27,8 @@ class PAOptimizer(IOptimizer):
         if variable.get_shape() != grad.shape:
             grad = grad.sum(axis=0)
         new_parameter = variable.get_value() - self.__optimizer.delta(grad / self.__batch_size)
-        self.__transfer.put_weights(new_parameter, variable.id, self.__block_mgr.batch_id, self.__block_mgr.current_block_id)
+        self.__transfer.put_weights(new_parameter, variable.id, self.__block_mgr.batch_id,
+                                    self.__block_mgr.current_block_id)
         if self.__block_mgr.end:
             new_parameter = self.__transfer.get_weights(variable.id, batch_no=self.__block_mgr.batch_id)
             variable.set_value(new_parameter)
