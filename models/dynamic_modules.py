ad5c752a89fc6344b19638c8c1e73128ac477e65
Engineer_DDP <zzaddp@live.com>
2020-10-27 11:54:04 +0800

    Reformat models.

---
+++ models/dynamic_modules.py
@@ -0,0 +1,47 @@
+import sys
+import importlib
+
+from models.interface import IReplyPackage
+
+
+Dynamic_Module_Name = "dynamic_modules"
+_spec = importlib.util.spec_from_loader(Dynamic_Module_Name, loader=None)
+_module = importlib.util.module_from_spec(_spec)
+sys.modules[Dynamic_Module_Name] = _module
+
+
+class ClassSerializer(IReplyPackage):
+
+    def __init__(self, cls_name: type):
+        # check module
+        execution_dirs = cls_name.__module__.split('.')
+        # if its from __main__
+        if execution_dirs[0] == '__main__':
+            import sys
+            mod_name = sys.argv[0]
+        else:
+            # get module filename
+            mod_name = "./" + "/".join(execution_dirs) + ".py"
+
+        self.__mod_name = mod_name
+        self.__class_name = cls_name.__name__
+        self.__mod_content = ''
+        with open(mod_name, 'r', encoding='utf-8') as file:
+            line = file.readline()
+            while line != '' and line[:26] != 'if __name__ == \'__main__\':':
+                self.__mod_content += line
+                line = file.readline()
+        self.__class_type: [type] = None
+
+    def restore(self) -> type:
+        definitions = compile(self.__mod_content, self.__mod_name, "exec", optimize=2)
+        exec(definitions, _module.__dict__)
+        cls_type = getattr(_module, self.__class_name)
+        self.__class_type = cls_type
+        return cls_type
+
+    def __call__(self, *params, **kwargs):
+        if self.__class_type:
+            return self.__class_type(*params, **kwargs)
+        else:
+            return None
---
+++ models/states.py
@@ -0,0 +1,35 @@
+from models.binary_file import BinaryFilePackage
+from models.interface import IReplyPackage
+
+
+class ReadyType:
+
+    def __init__(self, nodes_ready: set):
+        self.__nodes_ready = nodes_ready
+
+    def current_ready(self):
+        return self.__nodes_ready
+
+
+class DoneType(IReplyPackage):
+
+    def __init__(self, node_id, posted_files):
+        self.__node_id = node_id
+        self.__header = "./Node-{}-Retrieve/".format(node_id)
+        self.__contents = [BinaryFilePackage(f) for f in posted_files]
+
+    def restore(self) -> None:
+        for bf in self.__contents:
+            bf.filename = self.__header + bf.filename
+            bf.restore()
+
+    @property
+    def file_list(self):
+        for bf in self.__contents:
+            yield bf.filename
+
+    def __str__(self):
+        return "<Node({}) Reply: All Task is Completed.>".format(self.__node_id)
+
+    def __repr__(self):
+        return "<DoneType from Node({})>".format(self.__node_id)
