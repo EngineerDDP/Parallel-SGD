709e396b2b95ec6d54eb32fdcfc32d459e1af1fe
Engineer_DDP <zzaddp@live.com>
2021-06-02 18:21:40 +0800

    add UT

---
+++ nn/layer/conv2d_test.py
@@ -0,0 +1,15 @@
+import unittest
+
+import numpy as np
+
+from nn.layer.conv2d import Conv2D
+
+
+class TestConv(unittest.TestCase):
+
+    def test_conv(self):
+        x = np.ones(shape=[100, 32, 32, 3])
+        conv_layer = Conv2D(64, [3, 3])
+        conv_layer.F(x)
+        y = np.zeros(shape=[100, 30, 30, 64])
+        conv_layer.G(y)
---
+++ nn/model/model_test.py
@@ -0,0 +1,40 @@
+import os
+import unittest
+
+import nn
+from dataset import CIFAR
+from dataset.transforms import ImageCls
+
+
+class TestSequentialModel(unittest.TestCase):
+
+    def test_cifarnet(self):
+        os.chdir("../../")
+
+        model = nn.model.SequentialModel(input_shape=[-1, 32, 32, 3])
+        model.add(
+            nn.layer.Conv2D(kernel=64, kernel_size=[5, 5], activation=nn.activation.LeakReLU(leak_coefficient=0.2)))
+        model.add(
+            nn.layer.Conv2D(kernel=64, kernel_size=[5, 5], activation=nn.activation.LeakReLU(leak_coefficient=0.2)))
+        model.add(
+            nn.layer.Conv2D(kernel=64, kernel_size=[5, 5], activation=nn.activation.LeakReLU(leak_coefficient=0.2)))
+        model.add(
+            nn.layer.Conv2D(kernel=64, kernel_size=[5, 5], activation=nn.activation.LeakReLU(leak_coefficient=0.2)))
+        model.add(nn.layer.Flatten())
+        model.add(nn.layer.Dropout())
+        model.add(nn.layer.Dense(units=128, activation=nn.activation.Tanh()))
+        model.add(nn.layer.Dense(units=10, activation=nn.activation.Softmax()))
+
+        model.setup(nn.loss.Cross_Entropy_With_Softmax(), nn.metric.CategoricalAccuracy())
+        model.compile(nn.gradient_descent.ADAMOptimizer(alpha=0.0001))
+
+        model.save("CIFARNET.model")
+
+        trans = ImageCls()
+        x, y, x_t, y_t = trans(*CIFAR().load())
+
+        model.fit(x, label=y, epoch=1, batch_size=100)
+        res = model.evaluate(x, y)
+        self.assertGreater(res['accuracy'], 0.5)
+
+        os.remove("CIFARNET.model")
