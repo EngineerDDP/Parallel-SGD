7810c32fa422f23398cfd4a35c81ca9d358df7ca
Engineer_DDP <zzaddp@live.com>
2021-06-02 20:04:39 +0800

    remove unused statements

--- nn/layer/conv2d_test.py
+++ nn/layer/conv2d_test.py
@@ -10,6 +10,7 @@ class TestConv(unittest.TestCase):
     def test_conv(self):
         x = np.ones(shape=[100, 32, 32, 3])
         conv_layer = Conv2D(64, [3, 3])
-        conv_layer.F(x)
-        y = np.zeros(shape=[100, 30, 30, 64])
-        conv_layer.G(y)
+        for _ in range(100):
+            conv_layer.F(x)
+            y = np.zeros(shape=[100, 30, 30, 64])
+            conv_layer.G(y)
