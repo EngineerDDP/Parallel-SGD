6b139049fb4838e9b7fb3b0d04efd4809f1083c0
Engineer_DDP <zzaddp@live.com>
2021-06-08 12:03:06 +0800

    Add strides UT

--- nn/layer/conv2d_test.py
+++ nn/layer/conv2d_test.py
@@ -2,6 +2,7 @@ import unittest
 
 import numpy as np
 
+import nn
 from nn.layer.conv2d import Conv2D
 
 
@@ -14,3 +15,23 @@ class TestConv(unittest.TestCase):
             conv_layer.F(x)
             y = np.zeros(shape=[100, 30, 30, 64])
             conv_layer.G(y)
+
+    def test_strides(self):
+        x = nn.value.Placeholder(shape=[1, 32, 32, 3])
+        x.set_value(np.ones(shape=[1, 32, 32, 3]))
+        y = np.zeros(shape=[1, 28, 28, 1])
+        conv_layer = Conv2D(1, [5, 5], strides=(1, 1))
+        conv_layer.set_input(x)
+
+        conv_layer.F()
+        conv_layer.G(y)
+
+        self.assertEqual(x.get_gradient().shape, x.get_shape())
+
+        conv_layer = Conv2D(1, [5, 5], strides=(2, 2))
+        conv_layer.set_input(x)
+
+        conv_layer.F()
+        conv_layer.G(y)
+
+        self.assertEqual(x.get_gradient().shape, x.get_shape())
