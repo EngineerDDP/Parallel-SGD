f98909a54f6cf6e5c9c5efddeb962cb9cbbf115a
Engineer_DDP <zzaddp@live.com>
2021-06-08 12:16:08 +0800

    Add UT

--- nn/layer/maxpool_test.py
+++ nn/layer/maxpool_test.py
@@ -1,15 +1,35 @@
 import unittest
 
+import numpy as np
+
 import nn
 from nn.layer.maxpool import MaxPool
 
 
 class MyTestCase(unittest.TestCase):
+
     def test_normal(self):
-        x = nn.value.Variable(shape=(2, 5, 5, 1))
-        y = MaxPool([1, 1, 1, 1], "VALID", (2, 2), inputs=x)
+        x = nn.value.Variable(shape=(2, 6, 6, 1))
+        y = np.zeros(shape=(2, 3, 3, 1))
+
+        max_pool = MaxPool(strides=(2, 2), padding="VALID", size=(2, 2), inputs=x)
+
+        res = max_pool.F()
+        self.assertEqual(res.shape, (2, 3, 3, 1))
+
+        max_pool.G(y)
+        self.assertEqual(x.get_gradient().shape, x.get_shape())
+
+    def test_padding(self):
+        x = nn.value.Variable(shape=(2, 6, 6, 1))
+        y = MaxPool(strides=(2, 2), padding="SAME", size=(2, 2), inputs=x)
         self.assertEqual(y.F().shape, (2, 3, 3, 1))
 
+    def test_strides(self):
+        x = nn.value.Variable(shape=(2, 6, 6, 1))
+        y = MaxPool(strides=(3, 3), padding="SAME", size=(2, 2), inputs=x)
+
+        self.assertEqual(y.F().shape, (2, 2, 2, 1))
 
 if __name__ == '__main__':
     unittest.main()
