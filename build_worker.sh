19cde03543cf62aad606be13f563b69f19a009ff
Engineer_DDP <zzaddp@live.com>
2021-06-09 10:13:26 +0800

    Add parameter.

--- build_worker.sh
+++ build_worker.sh
@@ -1,25 +1,27 @@
-mkdir ./worker
-mkdir ./worker/codec
-mkdir ./worker/dataset
-mkdir -p ./worker/executor/psgd
+worker=$1
 
-cp ./codec/interfaces.py ./worker/codec/
-cp ./codec/__init__.py ./worker/codec/
-cp ./codec/essential.py ./worker/codec/
-cp -r ./dataset/transforms ./worker/dataset/
-cp ./dataset/interfaces.py ./worker/dataset/
-cp ./executor/psgd/net_package.py ./worker/executor/psgd/
-cp ./executor/__init__.py ./worker/executor/
-cp ./executor/abstract.py ./worker/executor/
-cp ./executor/interface.py ./worker/executor/
-cp -r ./models ./worker/
-cp -r ./network ./worker/
-cp -r ./nn ./worker/
-cp -r ./utils ./worker/
-cp -r ./psgd ./worker/
-cp -r ./roles ./worker/
-cp ./worker.py ./worker/
-cp ./constants.py ./worker/
-cp -r ./profiles ./worker
+mkdir "$worker"
+mkdir "$worker"/codec
+mkdir "$worker"/dataset
+mkdir -p "$worker"/executor/psgd
 
-python3 -m compileall ./worker/
+cp ./codec/interfaces.py "$worker"/codec/
+cp ./codec/__init__.py "$worker"/codec/
+cp ./codec/essential.py "$worker"/codec/
+cp -r ./dataset/transforms "$worker"/dataset/
+cp ./dataset/interfaces.py "$worker"/dataset/
+cp ./executor/psgd/net_package.py "$worker"/executor/psgd/
+cp ./executor/__init__.py "$worker"/executor/
+cp ./executor/abstract.py "$worker"/executor/
+cp ./executor/interface.py "$worker"/executor/
+cp -r ./models "$worker"/
+cp -r ./network "$worker"/
+cp -r ./nn "$worker"/
+cp -r ./utils "$worker"/
+cp -r ./psgd "$worker"/
+cp -r ./roles "$worker"/
+cp ./worker.py "$worker"/
+cp ./constants.py "$worker"/
+cp -r ./profiles "$worker"/
+
+python3 -m compileall "$worker"/
